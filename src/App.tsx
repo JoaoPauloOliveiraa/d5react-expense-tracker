b81e564378acf4568cd7f333ed27ca727b2b1ce0
João Paulo <joaopaulo.oliveira83@gmail.com>
2021-10-06 15:40:54 -0300

    Estrutura de exibição das despesas e estilização das categorias finalizadas.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import { Category } from './types/Category';
 import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
-
-
+import { TableArea } from './components/TableArea';
 
 const App = () =>{
   const [list, setList] = useState(items);
@@ -22,7 +21,11 @@ const App = () =>{
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
+
+
     <C.Body>
+        
+        <TableArea list={filteredList}/>
         
     </C.Body>
     </C.Container>
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,7 +1,7 @@
 import { Item } from "../types/Items";
 
 export const items: Item[] = [
-    {date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    {date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.12},
     {date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 25.20},
     {date: new Date(2021, 9, 23), category: 'rent', title: 'Aluguel Apt', value: 750},
     {date: new Date(2021, 10, 15), category: 'salary', title: 'Salário', value: 3070.58},
