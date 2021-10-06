75832cb569e3ad6a526de5714c3136f9765b907e
João Paulo <joaopaulo.oliveira83@gmail.com>
2021-10-06 00:14:26 -0300

    Função para filtrar o mês atual e retornar uma lista filtrada

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Items';
 import { Category } from './types/Category';
 import { categories } from './data/categories';
 import { items } from './data/items';
-
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 
 
 
 const App = () =>{
   const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect (()=>{
+    setFilteredList( filterListByMonth(list, currentMonth) );
+  }, [list, currentMonth]);
+
   return(
     <C.Container>
       <C.Header>
